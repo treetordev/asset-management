4861bc225b6b52db3097e2b01c73654fb987b240
Manish.mehts <manish.mehts@mynxsoftwares.com>
2025-03-27 09:35:54 +0530

    new code refactor

--- src/main/java/com/hrms/asset/management/response/AssetRequestResponse.java
+++ src/main/java/com/hrms/asset/management/response/AssetRequestResponse.java
@@ -1,6 +1,7 @@
 package com.hrms.asset.management.response;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 import com.hrms.asset.management.dao.Asset;
 
@@ -10,7 +11,7 @@ import lombok.Data;
 public class AssetRequestResponse {
 
     private Long id;
-    private Long employeeId;
+    private UUID employeeId;
     private Asset asset;
     private String comment;
     private LocalDate date;
